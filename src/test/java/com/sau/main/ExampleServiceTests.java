aeed6dd8ad5ac33171093000c0a5e7ccf8ded32a
saurabh <saurabh_khare@apple.com>
2018-05-02 15:13:46 -0700

    Initial commit

---
+++ src/test/java/com/sau/main/ExampleServiceTests.java
@@ -0,0 +1,14 @@
+package com.sau.main;
+
+import com.sau.main.ExampleService;
+import junit.framework.TestCase;
+
+public class ExampleServiceTests extends TestCase {
+
+	private ExampleService service = new ExampleService();
+	
+	public void testReadOnce() throws Exception {
+		assertEquals("Hello world!", service.getMessage());
+	}
+
+}
